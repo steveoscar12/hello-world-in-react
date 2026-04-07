017d2d26d1bf8faba36bb5823e0af1b316503c88
Steve <steve@example.com>
2026-04-08 02:47:26 +0600

    Initial commit: Basic React Hello World app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{color: 'blue', fontFamily: 'Times New Roman'}}>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
